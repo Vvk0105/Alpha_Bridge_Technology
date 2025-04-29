f9c4583fefe798b248f69f7f69161f5d3fd279ea
Vvk_0105 <vivekbabu.sics@gmail.com>
2025-04-30 01:10:11 +0530

    ignore added and till page 2 done

--- script.js
+++ script.js
@@ -1,33 +1,41 @@
-var page1Content = document.getElementById("page1-content");
-var cursor = document.getElementById("cursor");
+function cursorEffect(){
+    var page1Content = document.getElementById("page1-content");
+    var cursor = document.getElementById("cursor");
 
-page1Content.addEventListener("mousemove", function(e){
-    gsap.to(cursor, {
-        x: e.x,
-        y: e.y
+    page1Content.addEventListener("mousemove", function(e){
+        gsap.to(cursor, {
+            x: e.x,
+            y: e.y
+        })
     })
-})
 
-page1Content.addEventListener("mouseenter", function(){
-
-})
-
-page1Content.addEventListener("mouseleave", function(){
-    gsap.to(cursor, {
-        scale: 0
+    page1Content.addEventListener("mouseenter", function(){
+        gsap.to(cursor, {
+            scale: 1,
+            opacity: 1
+        })
     })
-})
+
+    page1Content.addEventListener("mouseleave", function(){
+        gsap.to(cursor, {
+            scale: 0,
+            opacity: 0,
+        })
+    })
 
 
 
-const lenis = new Lenis();
+    const lenis = new Lenis();
 
-lenis.on('scroll', ScrollTrigger.update);
-
-
-gsap.ticker.add((time) => {
-  lenis.raf(time * 1000); 
-});
+    lenis.on('scroll', ScrollTrigger.update);
 
 
-gsap.ticker.lagSmoothing(0);
+    gsap.ticker.add((time) => {
+    lenis.raf(time * 1000); 
+    });
+
+
+    gsap.ticker.lagSmoothing(0);
+}
+
+cursorEffect()
